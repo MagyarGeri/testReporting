3cb97ef8ab28807be301bcab712721d37ae10664
gerim <geri.magyar93@gmail.com>
2022-06-07 20:09:46 +0200

    headless on

--- src/test/java/YoutubeTest.java
+++ src/test/java/YoutubeTest.java
@@ -34,7 +34,7 @@ public class YoutubeTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-extensions");
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         options.addArguments("--window-size=1920,1080");
         options.addArguments("start-maximized","--incognito", "--lang=en");
         driver = new ChromeDriver(options);
